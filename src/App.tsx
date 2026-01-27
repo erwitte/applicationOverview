d57b96ed66957af80726c5a3dc0817ea51bfb4a5
erik witte <witte.erik2@gmail.com>
2026-01-27 23:50:05 +0100

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import Login from './Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <div className="min-h-screen 
                       w-full 
                       bg-slate-950 
                       flex 
                       justify-center 
                       items-center">
-        <Login></Login>
+                        <Routes>
+                          <Route path="/" element={<Login />} />
+                        </Routes>
       </div>
+    </BrowserRouter>
     </>
   )
 }
