a70f5b80326f5e505108df7cd1e0517cad15efaa
erik witte <witte.erik2@gmail.com>
2026-01-27 23:18:32 +0100

    added register button

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,7 +4,10 @@ function Login(){
             <div className="w-full max-w-md bg-slate-200 border border-slate-800 p-8 rounded-2xl shadow-2xl flex items-center justify-center flex-col">
                 <span>Username: </span> <input className="border border-black-200 rounded" type="text" placeholder="username..."></input>
                 <span>Password: </span> <input className="border border-black-200 rounded" type="password"></input>
+                <div className="flex gap-4">
                 <button className="border border-black-200 cursor-pointer rounded mt-5 p-1 bg-slate-400">Login</button>
+                <button className="border border-black-200 cursor-pointer rounded mt-5 p-1 bg-slate-400">Register</button>
+                </div>
             </div>
         </>
     )
