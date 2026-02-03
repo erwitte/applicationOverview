ceb263066067976347bbc10a4e46de0c2581c4fc
erik witte <witte.erik2@gmail.com>
2026-02-03 12:12:15 +0100

    log in works

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # React + TypeScript + Vite
 
+push to s3: aws s3 sync ./dist s3://application-overview --delete
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,16 +1,35 @@
 import ActionButton from "./components/ActionButton";
 import { useNavigate } from 'react-router-dom';
+import { signIn } from 'aws-amplify/auth';
+import { useState } from "react";
 
 function Login(){
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
     const navigate = useNavigate();
+    const handleLogIn = async () => {
+        try{
+            await signIn({
+            username: email,
+            password: password,
+            })
+            console.log("great success");
+        }
+        catch(e){
+            console.log("nope");
+        }
+    }
 
     return (
         <>
             <div className="w-full max-w-md bg-slate-200 border border-slate-800 p-8 rounded-2xl shadow-2xl flex items-center justify-center flex-col">
-                <span>Username: </span> <input className="border border-black-200 rounded" type="text" placeholder="username..."></input>
-                <span>Password: </span> <input className="border border-black-200 rounded" type="password"></input>
+                <span>E-Mail: </span> <input className="border border-black-200 rounded" type="text" placeholder="E-Mail..."
+                onChange={(e) => setEmail(e.target.value)}></input>
+                <span>Password: </span> <input className="border border-black-200 rounded" type="password"
+                onChange={(e) => setPassword(e.target.value)}></input>
                 <div className="flex gap-4 p-4">
-                    <ActionButton>Login</ActionButton>
+                    <ActionButton onClick={() => handleLogIn()}>Login</ActionButton>
                     <ActionButton onClick={() => navigate("/register")}>Register</ActionButton>
                 </div>
             </div>
